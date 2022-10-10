b2248d38a30191c18afd2431851a6a899594f318
Gleb Mirzayev <gmirzayev@gmail.com>
2022-10-10 17:10:01 -0400

    finish phase 4

--- skeleton/lib/p04_linked_list.rb
+++ skeleton/lib/p04_linked_list.rb
@@ -20,7 +20,12 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
   def initialize
+    @head = Node.new(:head)
+    @tail = Node.new(:tail)
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,30 +34,74 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next.key == :tail && @tail.prev.key == :head
   end
 
   def get(key)
+    self.each do |node|
+      return node.val if node.key == key
+    end
+    nil
+
+    # self.each {|node| }
   end
 
   def include?(key)
+    self.each do |node|
+      return true if node.key == key
+    end
+    false
   end
 
   def append(key, val)
+    prev = @tail.prev
+    node = Node.new(key, val)
+    node.prev = prev
+    node.next = @tail
+    prev.next = node
+    @tail.prev = node
   end
 
   def update(key, val)
+    self.each do |node|
+      node.val = val if node.key == key
+    end
   end
 
   def remove(key)
+    self.each do |node|
+      if node.key == key
+        prevNode = node.prev
+        nextNode = node.next
+        prevNode.next = nextNode
+        nextNode.prev = prevNode
+        break
+      end
+    end
   end
 
-  def each
+  def each(&prc)
+    #self.each do |node|
+    # p node.val
+    # end
+    node = @head.next
+    until node.key == :tail
+      prc.call(node)
+      node = node.next
+    end
+    self
+
+    #update
+    #while
+    #if key is the key you want, node.val 
   end
 
   # uncomment when you have `each` working and `Enumerable` included
@@ -60,3 +109,6 @@ class LinkedList
   #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
   # end
 end
+
+
+
