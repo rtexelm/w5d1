5a4b20eb9e8face5e272be1969d430f50f83e5ce
rtexelm <texelist@gmail.com>
2022-10-10 14:10:02 -0400

    Implemented InSizeSet

--- skeleton/lib/p01_int_set.rb
+++ skeleton/lib/p01_int_set.rb
@@ -1,19 +1,31 @@
 class MaxIntSet
+
+  attr_reader :store
+
   def initialize(max)
+    @store = Array.new(max, false)
   end
 
   def insert(num)
+    @store[num] = true if is_valid?(num)
   end
 
   def remove(num)
+    @store[num] = false if is_valid?(num)
   end
 
   def include?(num)
+    @store[num] if is_valid?(num)
   end
 
   private
 
   def is_valid?(num)
+    if num < 0 || num > @store.length - 1
+      raise "Out of bounds"
+    else
+      return true
+    end
   end
 
   def validate!(num)
@@ -27,18 +39,28 @@ class IntSet
   end
 
   def insert(num)
+    if !include?(num)
+      self[num] << num
+    end
   end
 
   def remove(num)
+    if include?(num)
+      index = self[num].index(num)
+      self[num].delete_at(index)
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    index = num % @store.length
+    @store[index]
   end
 
   def num_buckets
