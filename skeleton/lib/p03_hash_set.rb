3a71ba1606a9a6a5dac0940067b48dc6ccffd5e5
rtexelm <texelist@gmail.com>
2022-10-10 15:15:49 -0400

    Passed Hashset

--- skeleton/lib/p03_hash_set.rb
+++ skeleton/lib/p03_hash_set.rb
@@ -1,24 +1,44 @@
 class HashSet
   attr_reader :count
 
-  def initialize(num_buckets = 8)
+  def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
 
-  def insert(key)
+  def insert(num)
+    if !include?(num) && count 
+      if count + 1 == num_buckets
+        resize!
+        self[num] << num
+        @count += 1
+      else
+        self[num] << num
+        @count += 1
+      end
+    end
+
   end
 
-  def include?(key)
+  def remove(num)
+    if include?(num)
+      index = self[num].index(num)
+      self[num].delete_at(index)
+      @count -= 1
+    end
   end
 
-  def remove(key)
+  def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    hash = num.hash
+    index = hash % @store.length
+    @store[index]
   end
 
   def num_buckets
@@ -26,5 +46,11 @@ class HashSet
   end
 
   def resize!
+    old_store = @store.dup.flatten
+    @store = Array.new(num_buckets * 2) {Array.new}
+    
+    old_store.each do |num|
+      self[num] << num
+    end
   end
 end
